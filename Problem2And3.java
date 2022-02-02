ab9df2b940e6c5531424c7391ca20d22335fc6bd
balasasi <bala26292@gmail.com>
2022-02-02 16:46:50 +0530

    Second commit

---
+++ Problem2And3.java
@@ -0,0 +1,35 @@
+package org.login;
+
+import java.util.Scanner;
+
+public class Problem2And3 {
+public static void main(String[] args) {
+	Scanner s =new Scanner(System.in);
+	
+	int x=s.nextInt();
+	
+for ( int i = 0; i <= x; i++) 
+if(i%2==1 ) {
+	
+	
+	
+	System.out.print(" "+i);
+}
+}
+	
+
+
+
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
